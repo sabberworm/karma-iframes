8ec08e285d52c6436a4c1f662c5c9d9ecc680878
Raphael Schweikert <any@sabberworm.com>
2017-10-26 17:04:46 +0200

    Test that non-included files are not suddenly included to the iframe

--- test/included/test1.js
+++ test/included/test1.js
@@ -2,7 +2,7 @@ window['test1-global'] = 'defined';
 
 describe('globals', () => {
 	it('should only exist from test1 file', () => {
-	expect('test1-global' in window).to.be.true
-	expect('test2-global' in window).to.be.false
+		expect('test1-global' in window).to.be.true
+		expect('test2-global' in window).to.be.false
 	});
 });
---
+++ test/not-included/should-not-be-tested.js
@@ -0,0 +1,5 @@
+describe('this file', () => {
+	it('should not be included', () => {
+		expect('this file').to.be.undefined;
+	});
+});
