bb121d54bf0d353bf8df4eebcae2cc1a785d98b4
Raphael Schweikert <any@sabberworm.com>
2017-10-26 17:05:47 +0200

    Make sure tested feature works

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ let BROWSERS = [];
 let PLUGINS = [
 	'karma-mocha',
 	'karma-chai',
-	require('./index.js')	
+	require('./index.js')
 ]
 
 if('KARMA_BROWSER' in process.env) {
@@ -17,70 +17,75 @@ if('KARMA_BROWSER' in process.env) {
 }
 
 module.exports = function(config) {
-  config.set({
+	config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'iframes'],
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'test/**/*.js'
-    ],
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['mocha', 'chai', 'iframes'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+		// list of files / patterns to load in the browser
+		files: [
+			'test/included/**/*.js',
+			{
+				pattern: 'test/not-included/**/*.js',
+				included: false
+			}
+		],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'test/**/*.js': ['iframes']
-    },
+		// list of files to exclude
+		exclude: [
+		],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			'test/included/**/*.js': ['iframes'],
+			'test/not-included/**/*.js': [],
+		},
 
 
-    // web server port
-    port: 9876,
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+		// web server port
+		port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_DEBUG,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: BROWSERS,
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
+
+
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: BROWSERS,
 
 		plugins: PLUGINS,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity
+	})
 }
