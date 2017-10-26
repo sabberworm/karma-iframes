f146e90e82c665ca06a631c4db139e5ff61d05b3
Raphael Schweikert <any@sabberworm.com>
2017-10-26 19:46:21 +0200

    Test that non-included file is nonetheless served

--- test/included/test1.js
+++ test/included/test1.js
@@ -6,3 +6,9 @@ describe('globals', () => {
 		expect('test2-global' in window).to.be.false
 	});
 });
+
+describe('files', () => {
+	it('should have something to say about the non-included file', () => {
+		expect(window.__karma__.files).to.be.an('object').that.includes.all.keys('/base/test/not-included/should-not-be-tested.js');
+	});
+});
