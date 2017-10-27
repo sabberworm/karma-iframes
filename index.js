ea60eb71eeb7fb2fe8df9be7bb5a94d5e9444574
Raphael Schweikert <any@sabberworm.com>
2017-10-27 20:49:48 +0200

    Serve JS files instead of inlining them into the iframe

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports['framework:iframes'] = ['factory', require('./lib/framework.js').IFrameFramework];
-exports['middleware:iframes'] = ['factory', require('./lib/middleware.js')];
+exports['middleware:iframes-rewrite'] = ['factory', require('./lib/rewrite-middleware.js')];
+exports['middleware:iframes-serve-file'] = ['factory', require('./lib/serve-file-middleware.js').createStaticFileMiddleware];
 exports['preprocessor:iframes'] = ['factory', require('./lib/preprocessor.js')];
